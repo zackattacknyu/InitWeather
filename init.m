0fdba0a933c799fedbb648fe7e6e60b3bd0b7332
zackattacknyu <zackattacknyu@gmail.com>
2015-03-31 02:40:14 -0700

    more code improvements

--- init.m
+++ init.m
@@ -34,25 +34,7 @@ imgsToCompare = reshape(X2tr,[numImages 21 21]);
 
 %%
 
-%This does the comparison between a base image and a set of images
-% Input is baseImage, which has dimensions Height x Width
-%        imgsToCompare, which has dimensions NumImages x Height x Width
-
-numCompareImages = size(imgsToCompare,1);
-height = size(imgsToCompare,2);
-width = size(imgsToCompare,3);
-errors = zeros(1,numCompareImages);
-
-for j = 1:numCompareImages
-    
-    currentCompareImg = reshape(imgsToCompare(j,:,:),[height width]);
-    
-    %error is computed here
-    errors(j) = mseImage(baseImage,currentCompareImg);
-end
-
-[errorVals,bestImgs] = sort(errors);
-sortedImgs = imgsToCompare(bestImgs,:,:);
+[sortedImgs,~,~] = makeSortedImages(baseImage,imgsToCompare);
 
 figure
 subplot(4,4,1);
---
+++ makeSortedImages.m
@@ -0,0 +1,26 @@
+function [ sortedImgs,errorVals,bestImgs ] = makeSortedImages( baseImage,imgsToCompare )
+%MAKESORTEDIMAGES Summary of this function goes here
+%   Detailed explanation goes here
+
+%This does the comparison between a base image and a set of images
+% Input is baseImage, which has dimensions Height x Width
+%        imgsToCompare, which has dimensions NumImages x Height x Width
+
+numCompareImages = size(imgsToCompare,1);
+height = size(imgsToCompare,2);
+width = size(imgsToCompare,3);
+errors = zeros(1,numCompareImages);
+
+for j = 1:numCompareImages
+    
+    currentCompareImg = reshape(imgsToCompare(j,:,:),[height width]);
+    
+    %error is computed here
+    errors(j) = mseImage(baseImage,currentCompareImg);
+end
+
+[errorVals,bestImgs] = sort(errors);
+sortedImgs = imgsToCompare(bestImgs,:,:);
+
+end
+
