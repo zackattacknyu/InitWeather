e33e297a4aa148366518f9b958ddb881fd4c8bdc
zackattacknyu <zackattacknyu@gmail.com>
2015-04-30 01:40:01 -0700

    more emd tests done

--- emdTest.m
+++ emdTest.m
@@ -75,7 +75,51 @@ end
 
 %%
 
+patchSize=30;
+thresholdFactor=100;
+baseImageNum = 995;
+basePatch = reshape(newPatches(baseImageNum,:,:),[patchSize patchSize]);
+basePatchResized = imresize(basePatch, [10 10]);
+basePatchbinarized = basePatchResized>thresholdFactor;
+[baseWeight,basePixelLocs] = getFeatureWeight(basePatchbinarized);
 
+imageNums = [342 502 991 992 993 994 995 998];
+nImages = length(imageNums);
+patches = cell(1,nImages);
+patchesbinarized = cell(1,nImages);
+emdDists = zeros(1,nImages);
+curMaxPixel = 0;
+for i = 1:nImages
+    curPatch = reshape(newPatches(imageNums(i),:,:),[patchSize patchSize]);
+    patches{i} = curPatch;
+    curMaxPixel = max(max(max(curPatch)),curMaxPixel);
+    curPatchResize = imresize(curPatch, [10 10]);
+    curPatchbinarized = curPatchResize>thresholdFactor;
+    patchesbinarized{i} = curPatchbinarized;
+    [weight,pixelLocs] = getFeatureWeight(curPatchbinarized);
+    [~,f] = emd(pixelLocs,basePixelLocs,weight,baseWeight,@getPixelDist);
+    emdDists(i) = f;
+end
+
+[~,bestIndices] = sort(emdDists);
+%{
+patch1Resized = imresize(patch1,0.5);
+patch2Resized = imresize(patch2,0.5);
+patch1Resized2 = imresize(patch1,0.1);
+patch2Resized2 = imresize(patch2,0.1);
+%}
+
+figure
+for j=1:nImages
+   subplot(2,nImages,j);
+   imagesc(patches{bestIndices(j)},[0 curMaxPixel]);
+   axis image
+end
+for j=1:nImages
+   subplot(2,nImages,nImages + j);
+   imagesc(patchesbinarized{bestIndices(j)},[0 1]);
+   axis image
+end
 
 
 
