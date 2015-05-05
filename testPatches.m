6380ff0bce7bbbfc7ff46ea81eb3af16c0b9ab4c
zackattacknyu <zackattacknyu@gmail.com>
2015-05-04 19:01:27 -0700

    got images that are not arranged well in MSE

--- testPatches.m
+++ testPatches.m
@@ -1,37 +1,38 @@
 patchSize = 500;
-numPatches = 6;
+numPatches = 12;
 patches = cell(1,numPatches);
 patch1 = zeros(patchSize,patchSize);
 patch2 = zeros(patchSize,patchSize);
 
 center = floor([patchSize/2 patchSize/2]);
-centerVariation = 50;
+centerVariation = 150;
 centerVariants = floor(centerVariation*rand(numPatches,2));
 centers = repmat(center,numPatches,1) + centerVariants;
 centers(1,:) = center;
 centerVariants(1,:) = [0 0];
-radius = 100;
+radius = 50;
 
 for k = 1:numPatches
    newPatch = zeros(patchSize,patchSize);
+   curRadius = 50*rand(1,1);
    for i = 1:patchSize
        for j = 1:patchSize
            dist = norm([i j] - centers(k,:));
-          if( dist < radius)
-             newPatch(i,j) = dist/radius;
-          end
+           newPatch(i,j) = dist/curRadius;
        end
    end
     patches{k} = newPatch;
 end
 
+
 figure
 colormap bone;
 colorbar;
 for k = 1:numPatches
-   subplot(2,3,k);
+   subplot(3,4,k);
    imagesc(patches{k})
 end
+
 
 %%
 basePatch = patches{1};
@@ -43,6 +44,7 @@ for k = 1:numPatches
 end
 [orderedMSE,indices] = sort(mseVals);
 
+%{
 figure
 colormap bone;
 colorbar;
@@ -50,6 +52,7 @@ for k = 1:numPatches
    subplot(2,3,k);
    imagesc(patches{indices(k)})
 end
+%}
 
-%%
 distsFromCenter = sqrt(centerVariants(:,1).^2 + centerVariants(:,2).^2);
+distsFromCenter(indices)
