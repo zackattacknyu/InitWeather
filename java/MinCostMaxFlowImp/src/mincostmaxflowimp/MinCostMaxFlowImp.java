ddf55ed8b904d44a688f106512039d62f92fcc67
zackattacknyu <zackattacknyu@gmail.com>
2015-05-12 19:23:49 -0700

    got initial run of max flow alg

--- java/MinCostMaxFlowImp/src/mincostmaxflowimp/MinCostMaxFlowImp.java
+++ java/MinCostMaxFlowImp/src/mincostmaxflowimp/MinCostMaxFlowImp.java
@@ -17,6 +17,16 @@ public class MinCostMaxFlowImp {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        int[][] capacity = {{0,2,2,0},{2,0,0,2},{2,0,0,2},{0,2,2,0}};
+        int[][] cost = {{0,1,1,0},{1,0,0,1},{1,0,0,1},{0,1,1,0}};
+        int source = 0; int sink = 3;
+        MinCostMaxFlow nf = new MinCostMaxFlow();
+        int[] maxflow = nf.getMaxFlow(capacity,cost,source,sink);
+        
+        for(int j = 0; j < maxflow.length; j++){
+            System.out.println(maxflow[j]);
+        }
     }
     
 }
