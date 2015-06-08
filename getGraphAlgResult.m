3cf64865b3b329f7db7e26426987c12426d9fc5f
zackattacknyu <zackattacknyu@gmail.com>
2015-06-07 22:54:27 -0700

    work done

---
+++ getGraphAlgResult.m
@@ -0,0 +1,90 @@
+function [ xvals,fval ] = getGraphAlgResult( basePatch,curPatch )
+%GETGRAPHALGRESULT Summary of this function goes here
+%   Detailed explanation goes here
+
+javaaddpath('java/MinCostMaxFlowImp/build/classes')
+
+[baseWeight,basePixelLocs] = getFeatureWeight(basePatch);
+F1 = basePixelLocs;
+W1 = baseWeight;
+Func = @getPixelDist;
+[m a] = size(F1);
+   
+totalFlow = min(sum(sum(basePatch)),sum(sum(curPatch)));
+[weight,pixelLocs] = getFeatureWeight(curPatch);
+
+F2 = pixelLocs;
+W2 = weight;
+
+% number and length of feature vectors    
+[n a] = size(F2);
+
+% gets ground distance matrix
+f = zeros(m,n);
+capF = zeros(m,n); %capacity of link. set to zero.
+%maxDist = 5;
+for i = 1:m
+    for j = 1:n
+        f(i, j) = Func(F1(i, 1:a), F2(j, 1:a));
+        capF(i,j)=1;
+    end
+end
+
+%{
+Graph has the following description:
+    Edges from source to nodes with baseWeight
+    Edges between baseWeight and curWeight
+    Edges from curWeight to sink
+
+Let N be number of pixels in basePatch
+Cost Matrix and Capacity Matrix for the graph will have the following:
+    First N rows/columns are nodes for basePatch
+    Next N rows/columns are nodes for curPatch
+    Node 2N+1 is the source
+    Node 2N+1 is the sink
+%}
+
+N1 = length(W1); N2 = length(W2);
+matrixN = N1+N2+2;
+
+%makes cost matrix
+costMatrix = ones(matrixN,matrixN)*totalFlow;
+
+%makes capacity matrix. all edges not in graph have capacity 0
+capMatrix = zeros(matrixN,matrixN);
+
+%makes edges from source to base
+sourceIndex = N1+N2+1;
+for i = 1:N1
+   costMatrix(sourceIndex,i) = 0;
+   capMatrix(sourceIndex,i) = W1(i);
+end
+
+%makes edge from current to sink
+sinkIndex = sourceIndex+1;
+for i = 1:N2
+   index = N1 + i;
+   costMatrix(index,sinkIndex)=0;
+   capMatrix(index,sinkIndex)=W2(i);
+end
+
+%makes edges from base to current
+costMatrix(1:N1,(N1+1):(N2+N1)) = f;
+capMatrix(1:N1,(N1+1):(N2+N1)) = capF.*totalFlow;
+
+file1 = 'matricesToCompute/costMatrixTemp.txt';
+file2 = 'matricesToCompute/capMatrixTemp.txt';
+save(file1,'costMatrix','-ascii');
+save(file2,'capMatrix','-ascii');
+
+file1String = java.lang.String(strcat(pwd,'/',file1));
+file2String = java.lang.String(strcat(pwd,'/',file2));
+
+clear result;
+result = javaMethod('obtainEmdResults','mincostmaxflowimp.EmdResults',file1String,file2String);
+
+xvals = javaMethod('getPixelFlowMatrix',result);
+fval = javaMethod('getEmd',result);
+
+end
+
--- testMultiplesQP.m
+++ testMultiplesQP.m
@@ -1,7 +1,7 @@
 %%
 %save('qpTestPatches.mat','patches');
 %%
-load('qpTestPatches.mat');
+%load('qpTestPatches.mat');
 %%
 
 basePatch = patches{4};
@@ -9,38 +9,56 @@ basePatch = floor(abs(basePatch));
 
 xQP = cell(1,length(patches));
 fvalQP = cell(1,length(patches));
+xGraphAlg = cell(1,length(patches));
+fvalGraphAlg = cell(1,length(patches));
 
-for i = 1:20
+for i = 1:1
    
     i
     curPatch = patches{i};
     curPatch = floor(abs(curPatch));
+    
+    %THIS IS SOLID
+    [xx2,ff2] = getGraphAlgResult(basePatch,curPatch); 
+    
+    %THIS NEEDS TO CHANGE SOMEHOW
     [xx,ff] = getQuadProgResult(basePatch,curPatch);
+    
     
     xQP{i} = xx;
     fvalQP{i} = ff;
     
+    xGraphAlg{i} = xx2;
+    fvalGraphAlg{i} = ff2;
 end
 
 %%
-%{
+
 emdDistsQP = zeros(1,length(xQP));
+emdDistsGraph = zeros(1,length(xGraphAlg));
+numFlowVals = patchSize^4;
 for i = 1:length(xQP)
     flowVals = xQP{i};
-    flowVals = flowVals(1:10000);
-    emdDistsQP(i) = fvalQP{i}/sum(flowVals);
+    flowVals = flowVals(1:numFlowVals);
+    flowValsGraph = xGraphAlg{i};
+    flowValsGraph = flowValsGraph(:);
+    flowValsGraph = flowValsGraph(1:numFlowVals);
+    emdDistsQP(i) = sum(flowVals);
+    emdDistsGraph(i) = sum(flowValsGraph);
+    %emdDistsQP(i) = fvalQP{i}/sum(flowVals);
+    %emdDistsGraph(i) = fvalGraphAlg{i}/sum(flowValsGraph);
 end
 
 %%
 figure
-plot(sort(emdDistsWithPenSquared))
+plot(sort(emdDistsGraph))
 hold on
 plot(sort(emdDistsQP))
-legend('h1(f1star) + r0','h2(f2star)')
+legend('h1(f1star)','h1(f2star)')
 %%
 figure
 plot(emdDistsWithPenSquared)
-%}
-hold on
-plot(emdDistsQP)
-legend('h1(f1star) + r0','h2(f2star)')
+
+%hold on
+%plot(emdDistsQP)
+%legend('h1(f1star) + r0','h2(f2star)')
