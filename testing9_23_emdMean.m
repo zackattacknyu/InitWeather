b184107f3a99e9da13335302f3eed9a1cd66d48a
zackattacknyu <zackattacknyu@gmail.com>
2015-09-24 12:00:42 -0700

    more test

--- testing9_23_emdMean.m
+++ testing9_23_emdMean.m
@@ -12,8 +12,11 @@ numTestPatches = 3;
 testPatches = cell(1,numTestPatches);
 patches = zeros(numRC,numRC,numTestPatches);
 
+indices = randperm(length(testPatchesAll));
+
 for i = 1:numTestPatches
-    curPatch = floor(rand(numRC,numRC)*var) + minVal;
+    %curPatch = floor(rand(numRC,numRC)*var) + minVal;
+    curPatch = imresize(testPatchesAll{indices(i)},[numRC numRC]);
     testPatches{i} =  curPatch;
     patches(:,:,i) = curPatch;
 end
@@ -25,18 +28,25 @@ totalEMD = zeros(1,numT);
 
 diffsFromMean = cell(1,numT);
 
-meanRange = 20;
+meanRange = 16;
 
-for i = 1:numT
+vals = (-meanRange/2):meanRange/2;
+
+[xx,yy,zz] = meshgrid(vals,vals,vals);
+slot1 = xx(:);
+slot2 = yy(:);
+slot3 = zz(:);
+
+for i = 1:size(slot1)
     
     diffsFromMean{i} = diffFromMean;
     
-    curBase = initBasePatch+diffFromMean;
+    curBase = abs(initBasePatch+diffFromMean);
     
     emdCur = 0;
     
     for j = 1:numTestPatches
-       curPatch = testPatches{j}; 
+       curPatch = abs(testPatches{j}); 
         [~,rawF,~,~,totalFlow] = ...
             getQuadProgResult(curBase,curPatch,alphaVal);
         emdCur = emdCur + rawF/totalFlow;
@@ -44,5 +54,7 @@ for i = 1:numT
     
     totalEMD(i) = emdCur;
     
-    diffFromMean = abs(floor(rand(numRC,numRC)*meanRange - meanRange/2));
+    diffFromMean = [slot1(i) slot2(i);slot3(i) 0];
+    
+    %diffFromMean = abs(floor(rand(numRC,numRC)*meanRange - meanRange/2));
 end
